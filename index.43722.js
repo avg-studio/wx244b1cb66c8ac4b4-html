7920aa455ea4de53c8a5baac8a1922490bde6b93
t-nguyen <tunv@54vie.vn>
2026-06-01 13:29:06 +0700

    救救我鸭

--- index.43722.js
+++ index.43722.js
@@ -1,4 +1,4 @@
-System.register(["./application.28392.js"], function (_export, _context) {
+System.register(["./application.5b84d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
