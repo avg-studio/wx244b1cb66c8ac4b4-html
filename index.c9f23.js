c65facf8532136374e13cbff5317994e69ae0dc4
t-nguyen <tunv@54vie.vn>
2026-06-03 15:47:32 +0700

    fix bugs

--- index.c9f23.js
+++ index.c9f23.js
@@ -1,4 +1,4 @@
-System.register(["./application.64eb1.js"], function (_export, _context) {
+System.register(["./application.09571.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
